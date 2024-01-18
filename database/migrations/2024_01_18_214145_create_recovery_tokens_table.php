e21f6afe5a5576018d1d802876da888a483029c3
Hossein Mansouri <hsnmsri82@gmail.com>
2024-01-19 01:29:44 +0330

    create database migrations

---
+++ database/migrations/2024_01_18_214145_create_recovery_tokens_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecoveryTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('recovery_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('token');
+            $table->timestamp('expire_at');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recovery_tokens');
+    }
+}
---
+++ database/migrations/2024_01_18_214147_create_task_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('task_categories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_categories');
+    }
+}
