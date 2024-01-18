0cad203c7717439394168bc07d3e765402ded9bd
Hossein Mansouri <hsnmsri82@gmail.com>
2024-01-19 02:54:01 +0330

    defined models relation

--- app/Models/TaskCategories.php
+++ app/Models/TaskCategories.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class TaskCategories extends Model
 {
     use HasFactory;
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function task_categories()
+    {
+        return $this->hasMany(TaskCategories::class);
+    }
+
+    public function access_tokens()
+    {
+        return $this->hasMany(AccessToken::class);
+    }
+
+    public function recovery_tokens()
+    {
+        return $this->hasMany(RecoveryToken::class);
+    }
 }
