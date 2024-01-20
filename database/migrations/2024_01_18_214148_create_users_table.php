c8804bc254caef0dca40a52e0cafa45e0cb48eb4
Hossein Mansouri <hsnmsri82@gmail.com>
2024-01-20 22:13:27 +0330

    set nullable fields and create last_loggedin_at field

--- database/migrations/2024_01_18_214148_create_users_table.php
+++ database/migrations/2024_01_18_214148_create_users_table.php
@@ -17,14 +17,15 @@ class CreateUsersTable extends Migration
 
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('email');
             $table->bigInteger('role');
             $table->string('password');
-            $table->string('phone_number');
+            $table->string('phone_number')->nullable();
             $table->timestamp('created_at');
             $table->timestamp('updated_at');
+            $table->timestamp('last_loggedin_at')->nullable();
         });
 
         Schema::enableForeignKeyConstraints();
