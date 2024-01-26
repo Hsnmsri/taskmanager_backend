381fe8c3c90b10f63921b708766f2d269830891b
Hossein Mansouri <hsnmsri82@gmail.com>
2024-01-26 11:59:26 +0330

    set verify token for middleware and create verify token api

--- routes/api.php
+++ routes/api.php
@@ -20,33 +20,39 @@ use Illuminate\Support\Facades\Route;
 Route::post('auth/login', [UserController::class, 'login']);
 Route::post('auth/reset_password/request', [UserController::class, 'requestResetPassword']);
 Route::post('auth/reset_password/reset', [UserController::class, 'resetPassword']);
+Route::post('auth/verify_token', [UserController::class, 'checkUserToken']);
 
-// User
-Route::prefix('user')->group(function () {
-    Route::post('create', [UserController::class, 'create']);
-    Route::post('update', [UserController::class, 'update']);
-    Route::post('delete', [UserController::class, 'delete']);
-    Route::post('change_password', [UserController::class, 'changePassword']);
-    Route::post('change_email', [UserController::class, 'changeEmail']);
-    Route::post('data', [UserController::class, 'getData']);
-    Route::post('list', [UserController::class, 'getList']);
+Route::middleware(UserAuthentication::class)->group(function () {
+
+    // User
+    Route::prefix('user')->group(function () {
+        Route::post('create', [UserController::class, 'create']);
+        Route::post('update', [UserController::class, 'update']);
+        Route::post('delete', [UserController::class, 'delete']);
+        Route::post('change_password', [UserController::class, 'changePassword']);
+        Route::post('change_email', [UserController::class, 'changeEmail']);
+        Route::post('data', [UserController::class, 'getData']);
+        Route::post('list', [UserController::class, 'getList']);
+    });
+
+    // Task
+    Route::prefix('task')->group(function () {
+        Route::post('create', [TaskController::class, 'create']);
+        Route::post('update', [TaskController::class, 'update']);
+        Route::post('delete', [TaskController::class, 'delete']);
+        Route::post('done', [TaskController::class, 'taskDone']);
+        Route::post('restore', [TaskController::class, 'taskRestore']);
+        Route::post('list', [TaskController::class, 'getList']);
+    });
+
+    // Categories
+    Route::prefix('category')->group(function () {
+        Route::post('create', [TaskController::class, 'categoryCreate']);
+        Route::post('update', [TaskController::class, 'categoryUpdate']);
+        Route::post('delete', [TaskController::class, 'categoryDelete']);
+        Route::post('task_list', [TaskController::class, 'categoryTaskList']);
+        Route::post('list', [TaskController::class, 'categoryList']);
+    });
+
 });
 
-// Task
-Route::prefix('task')->group(function () {
-    Route::post('create', [TaskController::class, 'create']);
-    Route::post('update', [TaskController::class, 'update']);
-    Route::post('delete', [TaskController::class, 'delete']);
-    Route::post('done', [TaskController::class, 'taskDone']);
-    Route::post('restore', [TaskController::class, 'taskRestore']);
-    Route::post('list', [TaskController::class, 'getList']);
-});
-
-// Categories
-Route::prefix('category')->group(function () {
-    Route::post('create', [TaskController::class, 'categoryCreate']);
-    Route::post('update', [TaskController::class, 'categoryUpdate']);
-    Route::post('delete', [TaskController::class, 'categoryDelete']);
-    Route::post('task_list', [TaskController::class, 'categoryTaskList']);
-    Route::post('list', [TaskController::class, 'categoryList']);
-});
